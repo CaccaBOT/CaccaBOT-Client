0adf0a96a0dafaadb5a709558b69fc9f0dc02bd8
PolpOnline <git@t0mmy.anonaddy.com>
2024-06-18 22:57:36 +0200

    Unused imports

--- src/stores/global.ts
+++ src/stores/global.ts
@@ -1,6 +1,5 @@
 import { defineStore } from "pinia"
 import { useAPIStore } from "./api"
-import router from "../router/router"
 import { Leaderboard } from "../types/Leaderboard"
 import { Profile } from "../types/Profile"
 
--- src/stores/session.ts
+++ src/stores/session.ts
@@ -1,7 +1,4 @@
 import { defineStore } from "pinia"
-import { useAPIStore } from "./api"
-import router from "../router/router"
-import { Leaderboard } from "../types/Leaderboard"
 import { Session } from "../types/Session"
 
 export const useSessionStore = defineStore("session", {
